6e4715d3ad9ad586452aa9b6eca0a1efdbfaefd0
wmoodym <wmoody20@yahoo.com>
2025-08-22 13:22:06 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='abc-center-v2-1';
+const CACHE='abc-center-v2-1-1';
 const ASSETS=['/','/index.html','/signup.html','/site.webmanifest','/app/app.html','/app/app.js','/assets/icon-192.png','/assets/icon-256.png','/assets/icon-384.png','/assets/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
